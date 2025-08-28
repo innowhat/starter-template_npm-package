3b6ac452465327b6c589d5b09065eadb0353c274
inno <innowhat@gmail.com>
2025-08-28 17:45:43 +0300

    add preCommit
    Update package.json
    
    ok
    
    Update test.yml

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ export default defineConfig([
     {
         files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
         plugins: { js },
-        extends: ['js/recommended', 'prettier'],
+        extends: ['js/recommended'],
         languageOptions: { globals: globals.browser },
     },
     tseslint.configs.recommended,
