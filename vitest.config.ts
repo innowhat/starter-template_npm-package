c381e326e35aac4e7efe3c89b35b2e99b62965fb
Innocent Okeke <innowhat@gmail.com>
2025-08-28 15:25:04 +0300

    add vitest, add eslint and prettier formatter
    lint

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+    test: {
+        globals: true,
+        setupFiles: ['./vitest.setup.ts'],
+        exclude: ['node_modules', 'dist'],
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+            exclude: [
+                '**/tests/**',
+                '**/*.spec.tsx',
+                '**/*.test.tsx',
+                '**/vitest.setup.ts',
+            ],
+        },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+});
