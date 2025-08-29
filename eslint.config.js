a505d60843556fdbc18af681915c830744fe75d6
inno <innowhat@gmail.com>
2025-08-29 11:02:16 +0300

    Code clean up

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,35 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-export default defineConfig([
+export default [
     {
-        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-        plugins: { js },
-        extends: ['js/recommended'],
-        languageOptions: { globals: globals.browser },
+        files: ['**/*.{js,mjs,cjs,ts,tsx,mts,cts}'],
+        ignores: ['lib/**', 'build/**', 'dist/**', 'node_modules/**'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        rules: {
+            // JS recommended rules
+            ...js.configs.recommended.rules,
+
+            // Prettier formatting
+            'prettier/prettier': ['error', prettierConfig],
+
+            // Custom tweaks
+            'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+            'no-console': 'off',
+        },
     },
-    tseslint.configs.recommended,
-]);
+    ...tseslint.configs.recommended,
+];
